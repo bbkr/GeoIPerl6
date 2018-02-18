c682647322a1b0ef68b7f6680216608e4bebc412
Paweł bbkr Pabian <bbkr@post.pl>
2018-02-18 23:16:01 +0100

    modernize code a bit

--- lib/GeoIP/City.pm
+++ lib/GeoIP/City.pm
@@ -1,6 +1,6 @@
-unit class GeoIP::City:auth<github:bbkr>:ver<1.0.0>;
+unit class GeoIP::City:auth<github:bbkr>:ver<1.0.1>;
 
-use GeoIP:ver<1.0.0>;
+use GeoIP:ver<1.0.1>;
 
 has GeoIP %!db;
 
@@ -18,10 +18,10 @@ submethod BUILD ( Str :$directory where { .defined.not or .IO ~~ :d } ) {
         next unless GeoIP_db_avail( +$_ );
         
         # open database
-        %!db{$_} = GeoIP_open_type( +$_, 0 );
+        %!db{ $_ } = GeoIP_open_type( +$_, 0 );
         
         # set UTF-8 encoding
-        GeoIP_set_charset( %!db{$_}, 1 );
+        GeoIP_set_charset( %!db{ $_ }, 1 );
     }
     
     # clean up directory paths
@@ -39,30 +39,30 @@ method !locate ( Record $record! ) {
 
     my %result;
 
-    %result{'area_code'} = $_ with $record.area_code;
+    %result{ 'area_code' } = $_ with $record.area_code;
 
-    %result{'city'} = $_ with $record.city;
+    %result{ 'city' } = $_ with $record.city;
 
-    %result{'continent_code'} = $_ with $record.continent_code;
+    %result{ 'continent_code' } = $_ with $record.continent_code;
 
-    %result{'country'} = $_ with $record.country_name;
+    %result{ 'country' } = $_ with $record.country_name;
 
-    %result{'country_code'} = $_ with $record.country_code;
+    %result{ 'country_code' } = $_ with $record.country_code;
 
-    %result{'dma_code'} = $_ with $record.dma_code;
+    %result{ 'dma_code' } = $_ with $record.dma_code;
 
-    %result{'latitude'} = .round(0.000001) with $record.latitude;
+    %result{ 'latitude' } = .round( 0.000001 ) with $record.latitude;
 
-    %result{'longitude'} = .round(0.000001) with $record.longitude;
+    %result{ 'longitude' } = .round( 0.000001 ) with $record.longitude;
 
-    %result{'postal_code'} = $_ with $record.postal_code;
+    %result{ 'postal_code' } = $_ with $record.postal_code;
 
-    %result{'region'} = GeoIP_region_name_by_code( $record.country_code, $record.region )
+    %result{ 'region' } = GeoIP_region_name_by_code( $record.country_code, $record.region )
         if defined $record.country_code and defined $record.region;
 
-    %result{'region_code'} = $_ with $record.region;
+    %result{ 'region_code' } = $_ with $record.region;
 
-    %result{'time_zone'} = GeoIP_time_zone_by_country_and_region( $record.country_code, $record.region )
+    %result{ 'time_zone' } = GeoIP_time_zone_by_country_and_region( $record.country_code, $record.region )
         if defined $record.country_code and defined $record.region;
 
     return %result;
@@ -71,7 +71,7 @@ method !locate ( Record $record! ) {
 # locate by IPv4
 multi method locate ( Str:D $ip! where / ^ [\d ** 1..3] ** 4 % '.' $ / ) {
     
-    my $db = %!db{GEOIP_CITY_EDITION_REV1} or X::DatabaseMissing.new.throw( );
+    my $db = %!db{ GEOIP_CITY_EDITION_REV1 } or X::DatabaseMissing.new.throw( );
     
     return self!locate( GeoIP_record_by_addr( $db, $ip ) );
 }
@@ -79,7 +79,7 @@ multi method locate ( Str:D $ip! where / ^ [\d ** 1..3] ** 4 % '.' $ / ) {
 # locate by IPv6
 multi method locate ( Str:D $ip! where / ^ [<xdigit> ** 0..4] ** 3..8 % ':' $ / ) {
     
-    my $db = %!db{GEOIP_CITY_EDITION_REV1_V6} or X::DatabaseMissing.new.throw( );
+    my $db = %!db{ GEOIP_CITY_EDITION_REV1_V6 } or X::DatabaseMissing.new.throw( );
     
     return self!locate( GeoIP_record_by_addr_v6( $db, $ip ) );
 }
